912af7a139b0c3c480129eec4e49303c338c4ef2
Jan Koten <koten.jan.98@gmail.com>
2023-10-08 14:42:15 +0200

    improved UI

--- quizlet.py
+++ quizlet.py
@@ -2,112 +2,144 @@ import tkinter as tk
 from tkinter import ttk
 from PIL import Image, ImageTk
 
-questions_answers = []
+class QuestionApp(tk.Tk):
+    def __init__(self, directory):
+        super().__init__()
+        self.title("Question App")
+        self.geometry("1600x900")
+        self.configure(bg='#2E2E2E')
+        self.questions_answers = []
+        self.directory = directory
+        self._load_questions(f"{directory}/q.txt")
+        self.current_index = 0
+        self.img_label = None
+        self.showing_answer = False
+        self._setup_ui()
+        self.show_question()
 
-with open('questions.txt', 'r') as file:
-    lines = file.readlines()
-    for line in lines:
-        q, a = line.strip().split(';')
-        questions_answers.append((q, a))
+    def _load_questions(self, question_file):
+        with open(question_file, 'r') as file:
+            lines = file.readlines()
+            for line in lines:
+                q, a = line.strip().split(';')
+                self.questions_answers.append((q, a))
 
-current_index = 0
-img_label = None
-showing_answer = False
+    def show_question(self):
+        if self.current_index < len(self.questions_answers):
+            question_var.set(self.questions_answers[self.current_index][0])
+            result_var.set('')
+            if self.img_label:
+                self.img_label.pack_forget()
+            self.showing_answer = False
+        else:
+            question_var.set('No more questions!')
+            result_var.set("")
 
-def show_question():
-    global current_index, img_label, showing_answer
-    if current_index < len(questions_answers):
-        question_var.set(questions_answers[current_index][0])
-        result_var.set('')
-        if img_label:
-            img_label.pack_forget()
-        showing_answer = False
-    else:
-        question_var.set('No more questions!')
-        ok_btn.config(state=tk.DISABLED)
-        try_again_btn.config(state=tk.DISABLED)
-        result_var.set("")
+    def display_answer(self):
+        answer = self.questions_answers[self.current_index][1]
+        max_width = 1000  
+        max_height = 800  
 
-def display_answer():
-    global img_label, showing_answer
-    answer = questions_answers[current_index][1]
-    max_width = 1000  
-    max_height = 800  
+        if answer.lower().endswith(".png"):
+            if self.img_label:
+                self.img_label.pack_forget()
+            
+            with Image.open(f"{self.directory}/{answer}") as img:
+                if img.width > max_width or img.height > max_height:
+                    scale = min(max_width/img.width, max_height/img.height)
+                    new_width = int(img.width * scale)
+                    new_height = int(img.height * scale)
+                    img = img.resize((new_width, new_height))
+                photo = ImageTk.PhotoImage(img)
+            
+            self.img_label = ttk.Label(self, image=photo)
+            self.img_label.photo = photo  
+            self.img_label.pack(pady=20)
+        else:
+            result_var.set(answer)
+            if self.img_label:
+                self.img_label.pack_forget()
 
-    if answer.lower().endswith(".png"):
-        if img_label:
-            img_label.pack_forget()
+        self.showing_answer = True
+
+    def on_enter_pressed(self, event=None):
+        if self.showing_answer:
+            self.ok_clicked()
+        else:
+            self.display_answer()
+            
+    def on_backspace_pressed(self, event=None):
+        if self.showing_answer:
+            self.try_again_clicked()
+        else:
+            self.display_answer()
+
+    def ok_clicked(self):
+        # Hide labels
+        self.question_label.pack_forget()
+        self.result_label.pack_forget()
+        if self.img_label:
+            self.img_label.pack_forget()
+
+        self.configure(bg='green')
+        self.after(200, self._post_ok_click)
+
+    def _post_ok_click(self):
+        self.configure(bg='#2E2E2E')
+        # Show labels
+        self.question_label.pack(pady=30)
+        self.result_label.pack(pady=20)
+
+        self.current_index += 1
+        self.show_question()
+
+    def try_again_clicked(self):
+        # Hide labels
+        self.question_label.pack_forget()
+        self.result_label.pack_forget()
+        if self.img_label:
+            self.img_label.pack_forget()
+
+        self.configure(bg='red')
+        self.after(200, self._post_try_again_click)
+
+    def _post_try_again_click(self):
+        self.configure(bg='#2E2E2E')
+        # Show labels
+        self.question_label.pack(pady=30)
+        self.result_label.pack(pady=20)
+
+        if self.current_index + 10 < len(self.questions_answers):
+            self.questions_answers.insert(self.current_index + 10, self.questions_answers[self.current_index])
+        else:
+            self.questions_answers.append(self.questions_answers[self.current_index])
         
-        with Image.open(answer) as img:
-            if img.width > max_width or img.height > max_height:
-                scale = min(max_width/img.width, max_height/img.height)
-                new_width = int(img.width * scale)
-                new_height = int(img.height * scale)
-                img = img.resize((new_width, new_height))
-            photo = ImageTk.PhotoImage(img)
+        self.current_index += 1
+        self.show_question()
+
+    def _setup_ui(self):
+        style = ttk.Style(self)
+        style.configure('TButton', font=('Arial', 18, 'bold'), padding=10, background='#505050', foreground='white')
+        style.configure('TLabel', foreground="white", background="#2E2E2E", font=("Arial", 24))
+        style.configure('Question.TLabel', foreground="white", background="#2E2E2E", font=("Arial", 28))
+        style.configure('Answer.TLabel', foreground="white", background="#2E2E2E", font=("Arial", 18))
+
+        style.map('TButton', background=[('active', '#707070'), ('disabled', '#404040')], foreground=[('disabled', '#A0A0A0')])
+
+        global question_var, result_var, ok_btn, try_again_btn
         
-        img_label = ttk.Label(root, image=photo)
-        img_label.photo = photo  
-        img_label.pack(pady=20)
-    else:
-        result_var.set(answer)
-        if img_label:
-            img_label.pack_forget()
+        question_var = tk.StringVar(self)
+        result_var = tk.StringVar(self)
+        
+        self.question_label = ttk.Label(self, textvariable=question_var, style='Question.TLabel')
+        self.question_label.pack(pady=30)
 
-    showing_answer = True
+        self.result_label = ttk.Label(self, textvariable=result_var, style='Answer.TLabel')
+        self.result_label.pack(pady=20)
 
-def on_enter_pressed(event=None):
-    global current_index, showing_answer
-    if showing_answer:
-        ok_clicked()
-    else:
-        display_answer()
+        self.bind('<Return>', self.on_enter_pressed)
+        self.bind('<BackSpace>', self.on_backspace_pressed)
 
-def ok_clicked():
-    global current_index
-    current_index += 1
-    show_question()
-
-def try_again_clicked():
-    global current_index
-    if current_index + 10 < len(questions_answers):
-        questions_answers.insert(current_index + 10, questions_answers[current_index])
-    else:
-        questions_answers.append(questions_answers[current_index])
-    ok_clicked()
-
-root = tk.Tk()
-root.title("Question App")
-root.geometry("1600x900")
-root.configure(bg='#2E2E2E')
-
-style = ttk.Style(root)
-style.configure('TButton', font=('Arial', 18, 'bold'), padding=10, background='#505050', foreground='white')
-style.configure('TLabel', foreground="white", background="#2E2E2E", font=("Arial", 24))  # Increase font size here
-# Configure styles
-style.configure('Question.TLabel', foreground="white", background="#2E2E2E", font=("Arial", 28))  # Larger font for questions
-style.configure('Answer.TLabel', foreground="white", background="#2E2E2E", font=("Arial", 18))  # Smaller font for answers
-
-# Applying styles
-style.map('TButton', background=[('active', '#707070'), ('disabled', '#404040')], foreground=[('disabled', '#A0A0A0')])
-
-question_var = tk.StringVar(root)
-ttk.Label(root, textvariable=question_var, style='Question.TLabel').pack(pady=30)
-
-result_var = tk.StringVar(root)
-ttk.Label(root, textvariable=result_var, style='Answer.TLabel').pack(pady=20)
-
-button_frame = tk.Frame(root, bg='#2E2E2E')
-button_frame.pack(side=tk.BOTTOM, pady=20)
-
-ok_btn = ttk.Button(button_frame, text="OK", command=ok_clicked, state=tk.NORMAL)
-ok_btn.pack(side=tk.LEFT, padx=50)
-
-try_again_btn = ttk.Button(button_frame, text="Try Again", command=try_again_clicked, state=tk.NORMAL)
-try_again_btn.pack(side=tk.RIGHT, padx=50)
-
-root.bind('<Return>', on_enter_pressed)
-
-show_question()
-
-root.mainloop()
+if __name__ == "__main__":
+    app = QuestionApp('KOP')
+    app.mainloop()
